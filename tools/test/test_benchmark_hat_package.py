16a30d492ed63af1a0eb11dfc2f2554ed20dda00
Lisa Ong <onglisa@microsoft.com>
2022-01-12 17:04:19 +0800

    update test condition

--- tools/test/test_benchmark_hat_package.py
+++ tools/test/test_benchmark_hat_package.py
@@ -13,7 +13,7 @@ class BenchmarkHATPackage_test(unittest.TestCase):
     def setUp(self):
         self.hatfile_path = Path(os.path.dirname(__file__)) / "data" / get_platform().lower() / "optimized_matmul.hat"
 
-    @unittest.skipUnless(get_platform().lower() == "macos", "macOS is not supported by test")
+    @unittest.skipIf(get_platform().lower() == "macos", "macOS is not supported by test")
     def test_benchmark(self):
         run_benchmark(self.hatfile_path, store_in_hat=False, batch_size=2, min_time_in_sec=1, input_sets_minimum_size_MB=1)
 
