ba366beb355d3e46b496bb0673d35bd48380b350
Kern Handa <kern.handa@gmail.com>
2022-03-19 00:04:01 -0700

    .

--- hatlib/verify_hat_package.py
+++ hatlib/verify_hat_package.py
@@ -21,13 +21,13 @@ def verify_hat_package(hat_path):
 
         print("Inputs before function call:")
         for i, func_input in enumerate(func_inputs):
-            print(f"\tInput {i}: {','.join(func_input[:32])}")
+            print(f"\tInput {i}: {','.join(map(str, func_input[:32]))}")
 
         fn(inputs[name])
 
         print("Inputs after function call:")
         for i, func_input in enumerate(func_inputs):
-            print(f"\tInput {i}: {','.join(func_input[:32])}")
+            print(f"\tInput {i}: {','.join(map(str, func_input[:32]))}")
 
 
 def main():
