68809c2f858052eb31a94d54dcb2fc693e1dcde4
Lisa Ong <onglisa@microsoft.com>
2022-08-08 18:10:12 +0800

    support arbirtary pointer levels for declared types

--- hatlib/arg_info.py
+++ hatlib/arg_info.py
@@ -3,27 +3,28 @@ from functools import reduce
 import numpy as np
 import sys
 from dataclasses import dataclass
-from typing import Any, List, Tuple
+from typing import Any, List, Tuple, Union
 
 from . import hat_file
 
-# hat_declared_type : [ ctype, dtype_str ]
+# element_type : [ ctype, dtype_str ]
 ARG_TYPES = {
-    "int8_t*" : [ ctypes.c_int8, "int8" ],
-    "int16_t*" : [ ctypes.c_int16, "int16" ],
-    "int32_t*" : [ ctypes.c_int32, "int32" ],
-    "int64_t*" : [ ctypes.c_int64, "int64" ],
-    "uint8_t*" : [ ctypes.c_uint8, "uint8" ],
-    "uint16_t*" : [ ctypes.c_uint16, "uint16" ],
-    "uint32_t*" : [ ctypes.c_uint32, "uint32" ],
-    "uint64_t*" : [ ctypes.c_uint64, "uint64" ],
-    "float16_t*" : [ ctypes.c_uint16, "float16" ], # same bitwidth as uint16
-    "bfloat16_t*" : [ ctypes.c_uint16, "bfloat16" ],
-    "float*" : [ ctypes.c_float, "float32" ],
-    "double*" : [ ctypes.c_double, "float64" ],
+    "int8_t": [ctypes.c_int8, "int8"],
+    "int16_t": [ctypes.c_int16, "int16"],
+    "int32_t": [ctypes.c_int32, "int32"],
+    "int64_t": [ctypes.c_int64, "int64"],
+    "uint8_t": [ctypes.c_uint8, "uint8"],
+    "uint16_t": [ctypes.c_uint16, "uint16"],
+    "uint32_t": [ctypes.c_uint32, "uint32"],
+    "uint64_t": [ctypes.c_uint64, "uint64"],
+    "float16_t": [ctypes.c_uint16, "float16"],    # same bitwidth as uint16
+    "bfloat16_t": [ctypes.c_uint16, "bfloat16"],
+    "float": [ctypes.c_float, "float32"],
+    "double": [ctypes.c_double, "float64"],
 }
 CTYPE_ENTRY = 0
 DTYPE_ENTRY = 1
+
 
 @dataclass
 class ArgInfo:
@@ -35,8 +36,9 @@ class ArgInfo:
     element_num_bytes: int
     element_strides: Tuple[int, ...]
     total_element_count: int
-    total_byte_size: int
+    total_byte_size: Union[int, str]
     ctypes_pointer_type: Any
+    pointer_level: int
     usage: hat_file.UsageType = None
 
     def _get_type(self, type_str):
@@ -46,31 +48,44 @@ class ArgInfo:
 
         return np.dtype(type_str)
 
+    def _get_pointer_level(self, declared_type: str):
+        pos = declared_type.find("*")
+        if pos == -1:
+            return 0
+        return declared_type[pos:].count("*")
+
     def __init__(self, param_description: hat_file.Parameter):
         self.hat_declared_type = param_description.declared_type
         self.numpy_shape = tuple(param_description.shape)
         self.usage = param_description.usage
+        self.pointer_level = self._get_pointer_level(self.hat_declared_type)
+        element_type = self.hat_declared_type[:(-1 * self.pointer_level)] \
+            if self.pointer_level else self.hat_declared_type
 
-        if not self.hat_declared_type in ARG_TYPES:
-            raise NotImplementedError(f"Unsupported declared_type {self.hat_declared_type} in hat file")
+        if not element_type in ARG_TYPES:
+            raise NotImplementedError(f"Unsupported element_type {element_type} in hat file")
 
-        self.ctypes_pointer_type = ctypes.POINTER(ARG_TYPES[self.hat_declared_type][CTYPE_ENTRY])
-        dtype_entry = ARG_TYPES[self.hat_declared_type][DTYPE_ENTRY]
+        self.ctypes_pointer_type = ctypes.POINTER(ARG_TYPES[element_type][CTYPE_ENTRY])
+        dtype_entry = ARG_TYPES[element_type][DTYPE_ENTRY]
         self.numpy_dtype = self._get_type(dtype_entry)
         self.element_num_bytes = 2 if dtype_entry == "bfloat16" else self.numpy_dtype.itemsize
 
-        if self.numpy_shape:
-            self.element_strides = param_description.affine_map
-            self.numpy_strides = tuple([self.element_num_bytes * x for x in self.element_strides])
+        if param_description.logical_type == hat_file.ParameterType.AffineArray:
+            if self.numpy_shape:
+                self.element_strides = param_description.affine_map
+                self.numpy_strides = tuple([self.element_num_bytes * x for x in self.element_strides])
 
-            major_dim = self.element_strides.index(max(self.element_strides))
-            self.total_element_count = self.numpy_shape[major_dim] * self.element_strides[major_dim]
-            
+                major_dim = self.element_strides.index(max(self.element_strides))
+                self.total_element_count = self.numpy_shape[major_dim] * self.element_strides[major_dim]
+
+            else:
+                self.element_strides = self.numpy_strides = self.numpy_shape = [1]
+                self.total_element_count = 1
+            self.total_byte_size = self.element_num_bytes * self.total_element_count
+        elif param_description.logical_type == hat_file.ParameterType.RuntimeArray:
+            self.total_byte_size = f"{self.element_num_bytes} * {param_description.size}"
         else:
-            self.element_strides = self.numpy_strides = self.numpy_shape = [1]
-            self.total_element_count = 1
-
-        self.total_byte_size = self.element_num_bytes * self.total_element_count
+            raise NotImplementedError(f"Unspported logical_type {param_description.logical_type} in hat file")
 
 
 # TODO: Update this to take a HATFunction instead, instead of arg_infos and function_name
