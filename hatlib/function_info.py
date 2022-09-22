a793ef5d7a03ee924dc3894006474fcf442965c0
Lisa Ong <onglisa@microsoft.com>
2022-09-22 23:51:39 +0800

    expand arguments to include dimensions automatically

--- hatlib/function_info.py
+++ hatlib/function_info.py
@@ -1,9 +1,10 @@
+from turtle import shape
 import numpy as np
 import sys
 from dataclasses import dataclass, field
 from typing import Any, List
 
-from .arg_info import ArgInfo
+from .arg_info import ArgInfo, integer_like
 from .arg_value import ArgValue
 from . import hat_file
 
@@ -20,8 +21,50 @@ class FunctionInfo:
         self.arguments = list(map(ArgInfo, self.desc.arguments))
 
     def preprocess(self, args: List[Any]) -> List[ArgValue]:
-        # TODO: do a best effort expando of the arguments based on hat metadata
-        return args
+        if len(args) >= len(self.arguments):
+            return args # bail
+
+        if any(not isinstance(value, np.ndarray) for value in args):
+            return args # bail
+
+        # len(values) < len(self.arguments) and we have numpy arrays
+        # do a best effort parameter expansion based on hat metadata
+        value_idx = 0
+        expanded_args = [None] * len(self.arguments)
+        # maps argument names to indices
+        names_to_indices = {info.name: i for i, info in enumerate(self.arguments)}
+        for i, (hat_desc, info) in enumerate(zip(self.desc.arguments, self.arguments)):
+            array = args[value_idx]
+            if hat_desc.logical_type == hat_file.ParameterType.RuntimeArray:
+                if isinstance(array, np.ndarray):
+                    expanded_args[i] = array
+                    for dim_name, dim_val in zip(info.shape, array.shape):
+                        if integer_like(dim_name):
+                            assert int(dim_name) == int(dim_val)
+                            continue  # constant dimension
+
+                        # dynamic dimension, initialize a dimension ArgValue at its index
+                        dim_index = names_to_indices[dim_name]
+                        dim_arg_info = self.arguments[dim_index]
+                        dim_hat_desc = self.desc.arguments[dim_index]
+                        assert dim_hat_desc.logical_type == hat_file.ParameterType.Element
+
+                        if hat_desc.usage == hat_file.UsageType.Output:
+                            assert dim_hat_desc.usage == hat_file.UsageType.Output
+                            expanded_args[dim_index] = ArgValue(dim_arg_info)
+                        else:
+                            assert dim_hat_desc.usage == hat_file.UsageType.Input
+                            expanded_args[dim_index] = ArgValue(dim_arg_info, dim_val)
+                value_idx = value_idx + 1
+            elif hat_desc.logical_type == hat_file.ParameterType.AffineArray:
+                expanded_args[i] = array
+                value_idx = value_idx + 1
+            # else hat_file.ParameterType.Element handled above
+
+            if value_idx == len(args):
+                break  #  pre-processing complete
+
+        return expanded_args
 
     def verify(self, args: List[Any]):
         "Verifies that a list of argument values matches the function description"
